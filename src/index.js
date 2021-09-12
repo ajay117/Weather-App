c3f57095058e9cbe4453275596d6b1d4a6ba50a6
Ajay KC <meajay64@gmail.com>
2021-09-12 13:56:52 +0900

    Async function created to fetch data other API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-async function callApi() {
+// const input = document.querySelector('input');
+// const searchBtn = document.querySelector('#search-btn');
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = document.querySelector('#city-name');
+  // We will use this function to fetch lat, lon and other data from API.
+  // & use in when calling call().
+  const cityInfo = async () => {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&&appid=659000770aa7828200a70f67beaa5b6c`,
+      );
+      const info = await response.json();
+      const subData = info[0];
+      return [subData.name, subData.country, subData.lat, subData.lon];
+    } catch (err) {
+      // Later do something with this error.
+      console.log(err);
+    }
+  };
+  cityInfo();
+});
+
+// Callling API for weather data for specific location.
+const call = async () => {
   try {
     const response = await fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=pokhara&appid=659000770aa7828200a70f67beaa5b6c&units=imperial',
+      'https://api.openweathermap.org/data/2.5/onecall?lat=37.5683&lon=126.9778&exclude=minutely,hourly&appid=659000770aa7828200a70f67beaa5b6c',
       { mode: 'cors' },
     );
     const weatherData = await response.json();
@@ -9,8 +34,15 @@ async function callApi() {
   } catch (err) {
     return err;
   }
-}
-callApi();
+};
 
-// .then((response) => response.json())
-// .then((response) => console.log(response));
+call().then((data) => {
+  const section = document.createElement('section');
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
+  const div3 = document.createElement('div');
+  const para1 = document.createElement('p');
+  const para2 = document.createElement('p');
+  const para3 = document.createElement('p');
+  const para4 = document.createElement('p');
+});
