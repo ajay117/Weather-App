ab354117ac48c6203bd1cda10d8047e8ba878d48
Ajay KC <meajay64@gmail.com>
2021-09-12 21:43:14 +0900

    Fetch data load properly

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import mainShowcase from './modules/mainShowcase.js';
+// import populateForm from './modules/form.js';
 
 const form = document.querySelector('form');
-const main = document.querySelector('main');
+const showcase = document.querySelector('#showcase');
+
+// populateForm(showcase);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  showcase.innerHTML = '';
   const input = document.querySelector('#city-name');
   // We will use this function to fetch lat, lon and other data from API.
   // & use in when calling call().
@@ -29,12 +33,12 @@ form.addEventListener('submit', (e) => {
     try {
       const [cityName, countryName, lat, lon] = await cityInfo();
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}9778&exclude=minutely&appid=659000770aa7828200a70f67beaa5b6c`,
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}9778&exclude=minutely&units=metric&appid=659000770aa7828200a70f67beaa5b6c`,
         { mode: 'cors' },
       );
       const weatherData = await response.json();
       console.log(weatherData);
-      main.appendChild(mainShowcase(cityName, countryName, weatherData));
+      showcase.appendChild(mainShowcase(cityName, countryName, weatherData));
 
       // console.log(weatherData, weatherData.current);
       return weatherData;
