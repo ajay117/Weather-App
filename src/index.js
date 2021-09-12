d740132a0fded6f9181e055ee6fac1a7a97af3c1
Ajay KC <meajay64@gmail.com>
2021-09-12 17:04:05 +0900

    Fetch works all good now, and main showcase logic has done for dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-// const input = document.querySelector('input');
-// const searchBtn = document.querySelector('#search-btn');
+import mainShowcase from '../modules/mainShowcase.js';
+
 const form = document.querySelector('form');
+const main = document.querySelector('main');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const input = document.querySelector('#city-name');
@@ -8,41 +10,38 @@ form.addEventListener('submit', (e) => {
   // & use in when calling call().
   const cityInfo = async () => {
     try {
+      // import mainShowcase from '../modules/mainData.js';
       const response = await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&&appid=659000770aa7828200a70f67beaa5b6c`,
+        { mode: 'cors' },
       );
       const info = await response.json();
       const subData = info[0];
       return [subData.name, subData.country, subData.lat, subData.lon];
     } catch (err) {
       // Later do something with this error.
+      return err;
+    }
+  };
+
+  // Callling API for weather data for specific location.
+  const call = async () => {
+    try {
+      const [cityName, countryName, lat, lon] = await cityInfo();
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}9778&exclude=minutely,hourly&appid=659000770aa7828200a70f67beaa5b6c`,
+        { mode: 'cors' },
+      );
+      const weatherData = await response.json();
+      console.log(weatherData);
+      main.appendChild(mainShowcase(cityName, countryName, weatherData));
+
+      // console.log(weatherData, weatherData.current);
+      return weatherData;
+    } catch (err) {
       console.log(err);
     }
   };
-  cityInfo();
-});
 
-// Callling API for weather data for specific location.
-const call = async () => {
-  try {
-    const response = await fetch(
-      'https://api.openweathermap.org/data/2.5/onecall?lat=37.5683&lon=126.9778&exclude=minutely,hourly&appid=659000770aa7828200a70f67beaa5b6c',
-      { mode: 'cors' },
-    );
-    const weatherData = await response.json();
-    return weatherData;
-  } catch (err) {
-    return err;
-  }
-};
-
-call().then((data) => {
-  const section = document.createElement('section');
-  const div1 = document.createElement('div');
-  const div2 = document.createElement('div');
-  const div3 = document.createElement('div');
-  const para1 = document.createElement('p');
-  const para2 = document.createElement('p');
-  const para3 = document.createElement('p');
-  const para4 = document.createElement('p');
+  call();
 });
