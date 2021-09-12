05fe1c93e417d825769724eb3be9274c766001e7
Ajay KC <meajay64@gmail.com>
2021-09-12 21:02:36 +0900

    Hourly Data loads now

--- modules/mainShowcase.js
+++ modules/mainShowcase.js
@@ -1,28 +1,30 @@
+import populateByHour from './hourlyDataContainer.js';
+
 export default function mainShowcase(cityName, countryName, weatherData) {
-  const section = document.createElement('section');
-  const showcase = document.createElement('div');
-  const mainData = document.createElement('div');
-  const otherWeatherData = document.createElement('div');
-  const h4 = document.createElement('h4');
-  const tempInfo = document.createElement('p');
-  const humidityInfo = document.createElement('p');
-  const cloudInfo = document.createElement('p');
-  const img = document.createElement('img');
+  try {
+    const section = document.createElement('section');
+    const showcase = document.createElement('div');
+    const mainData = document.createElement('div');
+    const hourlyDataContainer = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const tempInfo = document.createElement('p');
+    const img = document.createElement('img');
 
-  h4.textContent = `${cityName}, ${countryName}`;
-  img.src = `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`;
-  //   img.src = 'http://openweathermap.org/img/wn/01n@2x.png';
-  img.alt = `${weatherData.current.weather.description} showing image`;
-  tempInfo.innerHTML = `Temperature: ${
-    weatherData.current.temp
-  }${String.fromCharCode(176)}`;
-  mainData.append(img, tempInfo);
+    h4.textContent = `${cityName}, ${countryName}`;
+    img.src = `http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`;
+    //   img.src = 'http://openweathermap.org/img/wn/01n@2x.png';
+    img.alt = `${weatherData.current.weather[0].description} showing image`;
+    tempInfo.innerHTML = `Temperature: ${
+      weatherData.current.temp
+    }${String.fromCharCode(176)}`;
+    mainData.append(img, tempInfo);
 
-  humidityInfo.textContent = `Humidity: ${weatherData.current.humidity}%`;
-  cloudInfo.textContent = `Cloud: ${weatherData.current.clouds}%`;
-  otherWeatherData.append(humidityInfo, cloudInfo);
+    populateByHour(weatherData, hourlyDataContainer);
 
-  showcase.append(mainData, otherWeatherData);
-  section.appendChild(showcase);
-  return section;
+    showcase.append(mainData, hourlyDataContainer);
+    section.append(h4, showcase);
+    return section;
+  } catch (err) {
+    console.log(err);
+  }
 }
